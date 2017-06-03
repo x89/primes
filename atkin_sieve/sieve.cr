7cf755cfa50309fddc61738323c7d3517de14571
David John <github@vaunt.eu>
2017-06-03 20:02:59 +0100

    Saturday night turn off brain time

--- atkin_sieve/sieve.cr
+++ atkin_sieve/sieve.cr
@@ -6,7 +6,7 @@ require "zip"
 # See: https://en.wikipedia.org/wiki/Sieve_of_Atkin#Algorithm
 # Written in the Crystal language
 
-max_prime = 100
+max_prime = 1000
 
 results = [2,3,5] of Int32
 
@@ -16,10 +16,9 @@ primes = {} of Int32 => Bool
   primes[n] = false
 end
 
-1.upto(max_prime) do |idx|
-  x = (1..max_prime).map { |x| x }
-  y = (1..max_prime).reject { |x| x % 2 == 0 }
-  # puts "#{x} #{y} #{x.zip(y)}"
+#  x = (1..max_prime).map { |x| x }
+  #y = (1..max_prime).reject { |x| x % 2 == 0 }
+  #puts "#{x} #{y} #{x.zip(y)}"
   # madzip = x.zip(y)
   #   .select{|x,y| y != nil}
   #   .map{|x,y| 4*x**2+y**2}
@@ -31,15 +30,29 @@ end
   #   end
   # end
 
-  x = (1..max_prime).reject { |x| x % 2 == 0 }
-  y = (1..max_prime).reject { |x| x % 2 != 0 }
-  puts "#{x.zip(y)}"
-  madzip = x.zip(y).map{|x,y| 4*x**2+y**2}.reject{|x| x > max_prime}
-  puts "#{madzip}"
-  madzip.each do |i|
-    if [7,19,31,43].includes? i % 60
-      primes[i] = !primes[i]
-    end
+x = (1..max_prime).reject { |x| x }
+y = (1..max_prime).reject { |x| x % 2 != 0 }
+madzip = (x+y).combinations(2).to_a.uniq
+  .map{ |x| 4 *  x[0]**2 * x[1]**2 }
+  .reject{ |x| x > max_prime || x < 1 }
+madzip.each do |i|
+  puts "#{i} #{i%60}"
+  if [1,13,17,29,37,41,49,53].includes? i % 60
+    primes[i] = !primes[i]
+    puts "flipped: #{i}"
+  end
+end
+
+x = (1..max_prime).reject { |x| x % 2 == 0 }
+y = (1..max_prime).reject { |x| x % 2 != 0 }
+madzip = (x+y).combinations(2).to_a.uniq
+  .map{ |x| 4 *  x[0]**2 * x[1]**2 }
+  .reject{ |x| x > max_prime || x < 1 }
+madzip.each do |i|
+  puts "#{i} #{i%60}"
+  if [7,19,31,43].includes? i % 60
+    primes[i] = !primes[i]
+    puts "flipped: #{i}"
   end
 end
 
