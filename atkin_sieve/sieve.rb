142a6cb544cfeb7c5db2b6f791d87ef4b9f57d36
David John <github@vaunt.eu>
2017-06-04 14:08:21 +0100

    Oh my god part 3.1 is working!

--- atkin_sieve/sieve.rb
+++ atkin_sieve/sieve.rb
@@ -5,51 +5,32 @@
 
 max_prime = 100
 
-results = [2,3,5] of Int32
+results = [2,3,5]
 
 # Fill a hash up to max_prime with everything set to false
-primes = Hash[(1..max_prime).map{ |i| [i,false] }]
+$primes = Hash[(1..max_prime).map{ |i| [i,false] }]
 
-#  x = (1..max_prime).map { |x| x }
-  #y = (1..max_prime).reject { |x| x % 2 == 0 }
-  #puts "#{x} #{y} #{x.zip(y)}"
-  # madzip = x.zip(y)
-  #   .select{|x,y| y != nil}
-  #   .map{|x,y| 4*x**2+y**2}
-  #   .reject{|x| x > max_prime}
-  # puts "#{madzip}"
-  # madzip.each do |i|
-  #   if [1,13,17,29,37,41,49,53].includes? n % 60
-  #     primes[n] = !primes[n]
-  #   end
-  # end
 
-x = (1..max_prime).reject { |x| x }
-y = (1..max_prime).reject { |x| x % 2 != 0 }
-madzip = (x+y).combinations(2).to_a.uniq
-  .map{ |x| 4 *  x[0]**2 * x[1]**2 }
-  .reject{ |x| x > max_prime || x < 1 }
-madzip.each do |i|
-  puts "#{i} #{i%60}"
-  if [1,13,17,29,37,41,49,53].includes? i % 60
-    primes[i] = !primes[i]
-    puts "flipped: #{i}"
+# 3.1 Flip for [1, 13, 17, 29, 37, 41, 49, 53] for 4x² + y² = n
+# x: all positive integers
+x = (1..max_prime).select { |x| x }
+# y: all odd integers
+y = (1..max_prime).select { |x| x % 2 != 0}
+res = (x+y).combination(2).to_a.uniq.map{|x,y| 4 * x**2 + y**2}
+res.reject! { |x| x > max_prime }
+res.each do |p|
+  if [1, 13, 17, 29, 37, 41, 49, 53].include? p then
+    $primes[p] = !$primes[p]
+    puts "Switched #{p}"
   end
 end
 
-x = (1..max_prime).reject { |x| x % 2 == 0 }
-y = (1..max_prime).reject { |x| x % 2 != 0 }
-madzip = (x+y).combinations(2).to_a.uniq
-  .map{ |x| 4 *  x[0]**2 * x[1]**2 }
-  .reject{ |x| x > max_prime || x < 1 }
-madzip.each do |i|
-  puts "#{i} #{i%60}"
-  if [7,19,31,43].includes? i % 60
-    primes[i] = !primes[i]
-    puts "flipped: #{i}"
-  end
-end
+# 3.2 Flip for [7, 19, 31, or 43] for 3x² + y² = n
+
+# 3.3 Flip for [11, 23, 47, 59] for 3x² - y² = n
 
 
-primes.reject! { |k,v| v == false }
-puts "#{primes}"
+puts "#{$primes}"
+$primes.reject! { |k,v| v == false }
+puts
+puts "#{$primes}"
