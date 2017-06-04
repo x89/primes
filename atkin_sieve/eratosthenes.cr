bcf093685869e69f8b5b3a84972eaa64961626df
David John <github@vaunt.eu>
2017-06-04 16:36:35 +0100

    Rewritted in Crystal! Benchmark results show about a 2x speedup and the 100m test ran unlike with Ruby.
            user     system      total        real
        0.000000   0.000000   0.000000 (  0.000059)
        0.000000   0.000000   0.000000 (  0.001362)
        0.350000   0.020000   0.370000 (  0.352664)
        4.820000   0.050000   4.870000 (  4.656557)
        33.520000   1.020000   34.540000 (  34.914995)

---
+++ atkin_sieve/eratosthenes.cr
@@ -0,0 +1,38 @@
+#!/usr/bin/env crystal
+
+# Implementation of the considerably simpler Sieve of Eratosthenes compared
+# to the Atkin sieve.
+# Used to generate up to N primes using the Crystal language.
+
+struct Int32
+  def eratosthenes
+    # Returns a hash of primes Int => Bool
+    # Where h[i] => true for prime numbers
+    n = self  # Because I prefer n
+    sqrt_n = Math.sqrt(n)
+    h = {} of Int32 => Bool
+    (1..n).each do |i|
+      h[i] = true
+    end
+    h[1] = false
+    (2..sqrt_n).each do |i|
+      if h[i] == true
+        (i*i..n).step(i).each do |j|
+          h[j] = false
+        end
+      end
+    end
+    h
+  end
+end
+
+# require "benchmark"
+#
+# runs = 1
+# Benchmark.bm do |x|
+#   x.report { runs.times do 100.eratosthenes; end }
+#   x.report { runs.times do 10000.eratosthenes; end }
+#   x.report { runs.times do 1000000.eratosthenes; end }
+#   x.report { runs.times do 10000000.eratosthenes; end }
+#   x.report { runs.times do 100000000.eratosthenes; end }
+# end
